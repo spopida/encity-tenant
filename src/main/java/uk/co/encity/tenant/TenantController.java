f193eabb75ff105ee14a134eceac2707aceb584a
Adrian Hall <ade@adrian-hall.name>
2021-01-16 00:12:08 +0000

    Initial implementation of json DTO schema validation for new tenant

--- src/main/java/uk/co/encity/tenant/TenantController.java
+++ src/main/java/uk/co/encity/tenant/TenantController.java
@@ -2,17 +2,21 @@ package uk.co.encity.tenant;
 
 import static com.mongodb.client.model.Filters.eq;
 
+import com.fasterxml.jackson.databind.jsonschema.JsonSerializableSchema;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.ValidationException;
+import org.everit.json.schema.loader.SchemaLoader;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 import reactor.util.Logger;
 import reactor.util.Loggers;
@@ -45,9 +49,49 @@ public class TenantController {
     }
 
     /**
+     * Attempt to create a new resource in the tenants collection
+     * @return  A Mono that wraps a ResponseEntity containing the response.  Possible
+     *          response status codes are OK, INTERNAL_SERVER_ERROR, BAD_REQUEST
+     */
+    @PostMapping("/tenants")
+    public Mono<ResponseEntity<String>> createTenant(@RequestBody String body) {
+        final String newTenantDTOSchemaName = "dto-schemas/new-tenant-dto.json";
+
+        logger.debug("Attempting to create a new tenant from request body:\n" + body);
+        ResponseEntity<String> response = null;
+
+        // TODO:
+        // - capture the body and check it against a JSON Schema Validator - possible 400 error
+        // - convert the body to a POJO (DTO)
+        // - check whether the tenancy already exists - if so, then client error (or internal error)
+        // - turn the POJO into an entity (aggregate) - add initial state (as part of a repeating group)
+        // - convert the entity to Bson / JSON
+        // - insert the entity in the datastore
+
+        try {
+            JSONObject jsonSchema = new JSONObject(
+                new JSONTokener(getClass().getClassLoader().getResourceAsStream(newTenantDTOSchemaName)));
+            JSONObject jsonSubject = new JSONObject(new JSONTokener(body));
+
+            Schema schema = SchemaLoader.load(jsonSchema);
+            schema.validate(jsonSubject);
+            logger.debug("Incoming request body validates against DTO schema");
+
+            response = ResponseEntity.status(HttpStatus.OK).body("{ key: \"Hi Adrian\" }");
+
+        } catch (ValidationException e) {
+            logger.warn("Incoming request body does NOT validate against DTO schema; potential API mis-use.");
+            response = ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+
+        return Mono.just(response);
+    }
+
+    /**
      * Attempt to get tenant info.  A tenant is identified by their internet domain.
      * @param domain the internet domain that identifies the tenancy
-     * @return A Mono that wraps a ResponseEntity containing the response
+     * @return  A Mono that wraps a ResponseEntity containing the response.  Possible
+     *          response status codes are INTERNAL_SERVER_ERROR, OK, and NOT_FOUND.
      */
     @GetMapping("/tenant/{domain}")
     public Mono<ResponseEntity<String>> getTenant(@PathVariable String domain) {
